9d2501f96c3415158b2f4d3d98db1cc9afe0bcfd
jlceb <jlceballosf@gmail.com>
2018-02-08 01:29:09 -0400

    Loginpage y la arquitectura del modelo

---
+++ Paises/Paises/Infrastructure/InstanceLocator.cs
@@ -0,0 +1,16 @@
+﻿
+namespace Paises.Infrastructure
+{
+    using ViewModels;
+
+    public class InstanceLocator
+    {
+        public LoginViewModel LvM { get; set; }
+
+        public InstanceLocator()
+        {
+            LvM = new LoginViewModel();
+        }
+
+    }
+}
